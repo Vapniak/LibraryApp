112564129fcc9d84b3202dbddb7e128a5ba0164e
Vapniak <113126917+Vapniak@users.noreply.github.com>
2023-10-16 16:37:39 +0200

    Starting rework on javascript logic

--- src/script.js
+++ src/script.js
@@ -1,27 +1,35 @@
-const myLibrary = [];
+const Library = [];
 
-const container = document.getElementById("container");
-
-let addButton = document.getElementById("add-item");
-
-addButton.addEventListener("click", function() {
-    addBookToLibrary(hobbit1);
-});
-
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `Title: ${title} by ${author}, ${pages} pages, ${read}`;
+class Book {
+    constructor(title, author, pages, read, id) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = id;
     }
 }
 
-function addBookToLibrary(book){
-    myLibrary.push(book);
+const uid = function(){
+    return Date.now().toString(36) + Math.random().toString(36);
+}
+
+let book = new Book("Hobbit", "Tolkien", 295, true, uid());
+
+const container = document.getElementById("container");
+
+const addItemButton = document.getElementById("add-item").addEventListener("click", function(){
+    addBook(book);
+});
+
+addBook(book);
+
+
+
+
+function addBook(book){
     const content = `
-    <div class="bg-zinc-950 rounded-lg p-4 min-w-min" array-index=${myLibrary.length}>
+    <div class="bg-zinc-950 rounded-lg p-4 min-w-min" id=${book.id}>
         <div class="flex flex-col gap-1">
             <p class="text-3xl font-semibold text-violet-500 mb-4">${book.title}</p>
             <p>Author: ${book.author}</p>
@@ -29,21 +37,14 @@ function addBookToLibrary(book){
             <p>Read: ${book.read}</p>
         </div>
         <div class="mt-6 flex justify-end">
-            <button type="button" onclick="removeBookFromLibrary()" class="bg-red-700 rounded-md p-2 hover:bg-red-900">Remove</button>
+            <button type="button" onclick="removeBook(this)" book-id=${book.id} class="bg-red-700 rounded-md p-2 hover:bg-red-900">Remove</button>
         </div>
-    </div>`
+    </div>`;
 
     container.innerHTML += content;
 }
 
-function removeBookFromLibrary(){
-    //remove
-    console.log("Test");
+function removeBook(sender){
+    let id = sender.getAttribute("book-id");
+    //find item by propert value id
 }
-
-
-let hobbit = new Book("Hobbit", "Tolkien", 295, true);
-let hobbit1 = new Book("Hobbit1", "Tolkien1", 2951, false);
-
-addBookToLibrary(hobbit);
-addBookToLibrary(hobbit1);
