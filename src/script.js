fefbc692f53e9f075218dccb576b3b10c30825b1
Vapniak <113126917+Vapniak@users.noreply.github.com>
2023-10-17 19:55:37 +0200

    Added pop up form to fill book info

--- src/script.js
+++ src/script.js
@@ -1,59 +1,74 @@
 const Library = [];
 
 class Book {
-    constructor(title, author, pages, read, id) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-        this.id = id;
-    }
+  constructor(title, author, pages, read, id) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+  }
 }
 let count = 0;
 
-const uid = function(){
-    return Date.now().toString(36) + Math.random().toString(36);
-}
-
-let book = new Book("Hobbit", "Tolkien", 295, "some", uid());
+const uid = function () {
+  return Date.now().toString(36) + Math.random().toString(36);
+};
 
 const container = document.getElementById("container");
+const popUpForm = document.getElementById("pop-up-form");
 
-const addItemButton = document.getElementById("add-item").addEventListener("click", function(){
-    addBook(book);
-});
+const addItemButton = document
+  .getElementById("add-item")
+  .addEventListener("click", function () {
+    showPopUpForm();
+  });
 
-addBook(book);
+function showPopUpForm(){
+  popUpForm.classList.remove("hidden");
+}
 
+function addBook() {
+  popUpForm.classList.add("hidden");
 
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").value;
 
-function addBook(book){
-    const content = `
+  let book = new Book(title, author, pages, read, uid());
+  const content = `
     <div class="bg-zinc-950 rounded-lg p-4 min-w-min" id=${book.id}>
         <div class="flex flex-col gap-1">
-            <p class="text-3xl font-semibold text-violet-500 mb-4">${book.title}</p>
+            <p class="text-3xl font-semibold text-violet-500 mb-4">${
+              book.title
+            }</p>
             <p>Author: ${book.author + count}</p>
             <p>Pages: ${book.pages}</p>
             <p>Read: ${book.read}</p>
         </div>
         <div class="mt-6 flex justify-end">
-            <button type="button" onclick="removeBook(this)" book-id=${book.id} class="bg-red-700 rounded-md p-2 hover:bg-red-900">Remove</button>
+            <button type="button" onclick="removeBook(this)" book-id=${
+              book.id
+            } class="bg-red-700 rounded-md p-2 hover:bg-red-900">Remove</button>
         </div>
     </div>`;
 
-    Library.push(book);
-    count++;
+  Library.push(book);
+  count++;
 
-    container.innerHTML += content;
+  container.innerHTML += content;
+
+  document.getElementById("form").reset();
 }
 
-function removeBook(sender){
-    let id = sender.getAttribute("book-id");
+function removeBook(sender) {
+  let id = sender.getAttribute("book-id");
 
-    let result = Library.find((item) => item.id === id);
+  let result = Library.find((item) => item.id === id);
 
-    let index = Library.indexOf(result);
-    Library.splice(index, 1);
+  let index = Library.indexOf(result);
+  Library.splice(index, 1);
 
-    document.getElementById(id).remove();
+  document.getElementById(id).remove();
 }
