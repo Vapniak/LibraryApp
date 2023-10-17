b9547f60d26a79a513a58c197374e5f646398e3b
Vapniak <113126917+Vapniak@users.noreply.github.com>
2023-10-17 17:58:03 +0200

    Added removing book functionality

--- src/script.js
+++ src/script.js
@@ -9,12 +9,13 @@ class Book {
         this.id = id;
     }
 }
+let count = 0;
 
 const uid = function(){
     return Date.now().toString(36) + Math.random().toString(36);
 }
 
-let book = new Book("Hobbit", "Tolkien", 295, true, uid());
+let book = new Book("Hobbit", "Tolkien", 295, "some", uid());
 
 const container = document.getElementById("container");
 
@@ -26,13 +27,12 @@ addBook(book);
 
 
 
-
 function addBook(book){
     const content = `
     <div class="bg-zinc-950 rounded-lg p-4 min-w-min" id=${book.id}>
         <div class="flex flex-col gap-1">
             <p class="text-3xl font-semibold text-violet-500 mb-4">${book.title}</p>
-            <p>Author: ${book.author}</p>
+            <p>Author: ${book.author + count}</p>
             <p>Pages: ${book.pages}</p>
             <p>Read: ${book.read}</p>
         </div>
@@ -41,10 +41,19 @@ function addBook(book){
         </div>
     </div>`;
 
+    Library.push(book);
+    count++;
+
     container.innerHTML += content;
 }
 
 function removeBook(sender){
     let id = sender.getAttribute("book-id");
-    //find item by propert value id
+
+    let result = Library.find((item) => item.id === id);
+
+    let index = Library.indexOf(result);
+    Library.splice(index, 1);
+
+    document.getElementById(id).remove();
 }
